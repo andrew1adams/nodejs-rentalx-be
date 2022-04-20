357cc8c6be2022084521b5934b96c1d4155e0b9a
Andrew Adams <andreewb02@gmail.com>
2022-04-20 08:23:33 -0300

    feat: add CreateSpecificationService

--- src/modules/cars/index.ts
+++ src/modules/cars/index.ts
@@ -1,6 +1,10 @@
 export * from './models/category'
+export * from './models/specification'
 
 export * from './repositories/categories-repository'
+export * from './repositories/specifications-repository'
 export * from './repositories/categories-repository-interface'
+export * from './repositories/specifications-repository-interface'
 
 export * from './services/create-category-service'
+export * from './services/create-specification-service'
---
+++ src/modules/cars/services/create-specification-service.ts
@@ -0,0 +1,24 @@
+import {
+  ISpecificationsRepositoryParams,
+  ISpecificationsRepository
+} from '~/modules/cars'
+
+class CreateSpecificationService {
+  private specificationsRepository: ISpecificationsRepository
+
+  constructor(specificationsRepository: ISpecificationsRepository) {
+    this.specificationsRepository = specificationsRepository
+  }
+
+  execute({ name, description }: ISpecificationsRepositoryParams): void {
+    const SpecificationAlreadyExists =
+      this.specificationsRepository.findByName(name)
+
+    if (SpecificationAlreadyExists)
+      throw new Error('Specification already exists')
+
+    this.specificationsRepository.create({ name, description })
+  }
+}
+
+export { CreateSpecificationService }
