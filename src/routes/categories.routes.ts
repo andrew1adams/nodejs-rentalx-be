282df1736be9166cb22c3e63ab40172e5b40dff9
Andrew Adams <andreewb02@gmail.com>
2022-04-25 23:15:16 -0300

    feat: add ImportCategory route on server

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express'
 import {
   createCategoryController,
-  listCategoriesController
+  listCategoriesController,
+  importCategoryController
 } from '~/modules/cars'
+import multer from 'multer'
 
 const categoriesRoutes = Router()
+
+const upload = multer({ dest: 'tmp' })
 
 categoriesRoutes.post('/', (req, res) =>
   createCategoryController.handle(req, res)
@@ -13,5 +17,9 @@ categoriesRoutes.post('/', (req, res) =>
 categoriesRoutes.get('/', (_req, res) =>
   listCategoriesController.handle(_req, res)
 )
+
+categoriesRoutes.post('/import', upload.single('file'), (req, res) => {
+  return importCategoryController.handle(req, res)
+})
 
 export { categoriesRoutes }
