362691c39fb7a1355bb981ff468da8e025f40abf
Andrew Adams <andreewb02@gmail.com>
2022-04-25 23:21:37 -0300

    feat: start swagger

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,13 @@
 import express from 'express'
+import swagger from 'swagger-ui-express'
 import { main } from '~/routes'
+import swaggerConfig from './swagger.json'
 
 const app = express()
 
 app.use(express.json())
+
+app.use('/api-docs/', swagger.serve, swagger.setup(swaggerConfig))
 
 app.use(main)
 
